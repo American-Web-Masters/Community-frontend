9df25f177c955525c60085860f83504b55099484
ahad <engrabdulahad924@gmail.com>
2026-04-10 19:57:31 +0500

    [FEAT]: added routes to help center and also added help center option in settings for navigation

--- src/pages/help_center/subcomponents/index.js
+++ src/pages/help_center/subcomponents/index.js
@@ -1,3 +1,4 @@
+// export to help center main page
 export { HelpCenterMainTabs, HelpCenterCategoryTabs } from "./HelpCenterTabs";
 export { default as HelpCenterAccordionItem } from "./HelpCenterAccordionItem";
 export { default as HelpCenterForumTab } from "./HelpCenterForumTab";
