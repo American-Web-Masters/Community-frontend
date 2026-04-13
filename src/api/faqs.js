f43cd11e6381f42db8632d71b73b796c3c406fd8
ahad <engrabdulahad924@gmail.com>
2026-04-13 23:09:06 +0500

    [FEAT]: implemented dynamic filtering of faqs according to category

--- src/api/faqs.js
+++ src/api/faqs.js
@@ -2,7 +2,7 @@ import apiClient from "./client.js";
 
 /**
  * Get all FAQs.
- * Expected endpoint: GET /api/v1/faqs/
+ * Expected endpoint: GET /api/v1/faqs
  */
 export const getAllFaqs = async () => {
   const response = await apiClient.get("/faqs/");
